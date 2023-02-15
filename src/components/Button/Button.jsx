770a45fc26e1b6f4b242c9585f6cc49bd840dd9d
mikalojuszrg <mikalojus.zirgulis@gmail.com>
2023-02-15 15:10:20 +0200

    button styles added

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,5 +1,16 @@
-const Button = ({ children, onClick }) => {
-  return <button onClick={onClick}>{children}</button>;
+import styles from "./Button.module.scss";
+import { buttonClassNames } from "../../consts/buttonClassNames";
+import classNames from "classnames";
+
+const cn = classNames.bind(styles);
+
+const Button = ({ children, onClick, variant }) => {
+  const classes = buttonClassNames[variant];
+  return (
+    <button onClick={onClick} className={cn(...classes)}>
+      {children}
+    </button>
+  );
 };
 
 export default Button;

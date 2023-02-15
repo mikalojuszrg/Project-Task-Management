44bc482fd8c7ed5a881ed7986737d85c27c3e80d
mikalojuszrg <mikalojus.zirgulis@gmail.com>
2023-02-15 15:11:16 +0200

    consts/utils

---
+++ src/consts/buttonClassNames.js
@@ -0,0 +1,7 @@
+import styles from "../components/Button/Button.module.scss";
+
+export const buttonClassNames = {
+  primary: [styles["button--primary"], styles["button"]],
+  secondary: [styles["button--secondary"], styles["button"]],
+  tertiary: [styles["button--tertiary"], styles["button"]],
+};
